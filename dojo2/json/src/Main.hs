a3256f8aabd7e93b2c8bbaacdc62df62dc75202b
Janne Hellsten <jjhellst@gmail.com>
2012-10-31 23:19:23 +0200

    Example of how to do the same with the 'json' library

--- dojo2/json/src/Main.hs
+++ dojo2/json/src/Main.hs
@@ -1,28 +1,12 @@
 
+import           Control.Applicative
 import           Data.Maybe (fromMaybe)
 import qualified Data.Map as M
+import           Data.Maybe (fromMaybe)
+import           Text.JSON
+
 import           Types
 import qualified YaJSON as Y
-
-testInputs :: [String]
-testInputs =
-  [ "{ \"suit\": \"hearts\", \"rank\": 1 }"
-  , "{ \"suit\": \"spades\", \"rank\": 2 }"
-  , "{ \"suit\": \"diamonds\", \"rank\": 3 }"
-  , "{ \"suit\": \"clubs\", \"rank\": 4 }"
-  ]
-
--- missing "suit"
-testErr1 :: String
-testErr1 = "{ \"rank\": 1 }"
-
--- misspelt "suit" content 'darts'
-testErr2 :: String
-testErr2 = "{ \"suit\": \"darts\", \"rank\": 1 }"
-
--- rank content has a string and not an int
-testErr3 :: String
-testErr3 = "{ \"suit\": \"spades\", \"rank\": \"1\" }"
 
 toSuit :: String -> Either String Suit
 toSuit "hearts"   = Right Hearts
@@ -55,9 +39,9 @@ parseCardYaJSON s =
                 Left err -> error err
                 Right s' -> Card s' r
             Just _ -> error "rank must be an int"
-            Nothing -> error "no rank element in input"
-        Just _ -> error "suit must be an int"
-        Nothing -> error "no suit element in input"
+            Nothing -> error "no 'rank' element in input"
+        Just _ -> error "suit must be a string"
+        Nothing -> error "no 'suit' element in input"
 
     jsonToCard _ = error "malformed structure"
 
@@ -71,7 +55,7 @@ tryTakeElement :: String -> M.Map String Y.JSON -> Either String Y.JSON
 tryTakeElement eltName dict =
   maybe
     (Left ("missing elt '"++eltName++"'"))
-    Right
+     Right
     (M.lookup eltName dict)
 
 tryTakeString :: String -> M.Map String Y.JSON -> Either String String
@@ -100,6 +84,47 @@ parseCardYaJSONImproved s =
       rank <- tryTakeInt "rank" d
       return $ Card suit rank
 
+------------------------------------------------------------------
+
+-- Using the 'json' library
+--
+-- Note how JSON parsing and "semantic" errors (invalid suit) are
+-- handled.
+--
+-- There is a sufficient amount of type magic going on here.  For
+-- example, 'rank' conversion to Int happens automatically.
+instance JSON Card where
+  showJSON _ = error "unimplemented"
+  readJSON object = do
+    obj <- readJSON object
+    Card <$> (valFromObj "suit" obj >>= toSuitJSON)
+         <*>  valFromObj "rank" obj
+    where
+      toSuitJSON s = either Error Ok (toSuit s)
+
+decodeCard :: String -> Result Card
+decodeCard = decode
+
+------------------------------------------------------------------
+testInputs :: [String]
+testInputs =
+  [ "{ \"suit\": \"hearts\", \"rank\": 1 }"
+  , "{ \"suit\": \"spades\", \"rank\": 2 }"
+  , "{ \"suit\": \"diamonds\", \"rank\": 3 }"
+  , "{ \"suit\": \"clubs\", \"rank\": 4 }"
+  ]
+
+-- missing "suit"
+testErr1 :: String
+testErr1 = "{ \"rank\": 1 }"
+
+-- misspelt "suit" content 'darts'
+testErr2 :: String
+testErr2 = "{ \"suit\": \"darts\", \"rank\": 1 }"
+
+-- rank content has a string and not an int
+testErr3 :: String
+testErr3 = "{ \"suit\": \"spades\", \"rank\": \"1\" }"
 
 main :: IO ()
 main = do
@@ -111,3 +136,8 @@ main = do
   print $ parseCardYaJSONImproved testErr1
   print $ parseCardYaJSONImproved testErr2
   print $ parseCardYaJSONImproved testErr3
+  putStrLn "same with the 'json' library"
+  mapM_ (print . decodeCard) testInputs
+  print $ decodeCard testErr1
+  print $ decodeCard testErr2
+  print $ decodeCard testErr3
