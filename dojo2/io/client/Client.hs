6a57c6248ca00a76933e8eca8a8c56baebcc3908
Janne Hellsten <jjhellst@gmail.com>
2012-12-11 23:12:18 +0200

    Add player + card encode/decode

--- dojo2/io/client/Client.hs
+++ dojo2/io/client/Client.hs
@@ -2,15 +2,24 @@
 import Control.Exception (bracket)
 import Network
 import System.IO
+import Text.JSON
+
+import Types
 
 server = "localhost"
 port   = 4242
+
+player :: Player
+player = Player "Jopex"
 
 main :: IO ()
 main = do
   bracket (connectTo server (PortNumber (fromIntegral port))) hClose run
   where
     run h = do
-      hSetBuffering h NoBuffering
-      t <- hGetContents h
-      putStrLn $ "server sez: " ++ t
+      hSetBuffering h LineBuffering
+      hPutStrLn h $ encode player
+      card <- fmap decodeCard (hGetContents h)
+      either (\e -> putStrLn $ "error: " ++ show e)
+             (\c -> putStrLn $ "got card: " ++ show c)
+             card
